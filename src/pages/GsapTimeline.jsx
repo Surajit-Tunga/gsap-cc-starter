0e7c51910e2a7e8fbb5f9627465546e869a54a2d
Surajit Tunga <surajittunga2005@gmail.com>
2025-06-26 19:05:12 +0530

    done

--- src/pages/GsapTimeline.jsx
+++ src/pages/GsapTimeline.jsx
@@ -10,7 +10,35 @@ const GsapTimeline = () => {
   });
 
   
+useGSAP(()=> {
+  timeline.to('#yellow-box', {
+    x:250,
+    rotation:360,
+    borderRadius:'100%',
+    duration:2,
+    ease: "back.inOut"
 
+  })
+
+  timeline.to('#yellow-box', {
+    y:100,
+    rotation:360,
+    borderRadius:'100%',
+    duration:.5,
+    ease: "back.inOut"
+
+  })
+
+  timeline.to('#yellow-box', {
+    x:500,
+    rotation:360,
+    borderRadius:'8px',
+    duration:2,
+    ease: "back.inOut"
+
+  })
+
+}, []);
 
   return (
     <main>
@@ -46,7 +74,13 @@ const GsapTimeline = () => {
       </p>
 
       <div className="mt-20 space-y-10">
-        <button onClick={() => {}}>Play/Pause</button>
+        <button onClick={() => {
+          if(timeline.paused()) {
+            timeline.play();
+          } else {
+            timeline.pause();
+          }
+        }}>Play/Pause</button>
 
         <div id="yellow-box" className="w-20 h-20 bg-yellow-500 rounded-lg" />
       </div>
